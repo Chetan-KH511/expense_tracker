7cc53150a473efb2ec94b8b5634debf1ce3fed58
ChetanB04 <chetanbhat645@gmail.com>
2024-02-05 19:07:54 +0530

    Changed the colorthemes

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,35 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var colorskeme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: colorskeme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: colorskeme.onPrimaryContainer,
+          foregroundColor: colorskeme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: colorskeme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: colorskeme.primaryContainer),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: colorskeme.onSecondaryContainer,
+                fontSize: 14,
+              ),
+            ),
+      ),
       home: Expenses(),
     ),
   );
